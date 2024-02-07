1e0083a89c6fa63592d9af3393e477bcd3577302
Dzaki <dzakigunawan34@gmail.com>
2024-02-07 15:19:38 +0700

    1.6.5

--- Express/controllers/controller.js
+++ Express/controllers/controller.js
@@ -2,8 +2,8 @@ const siswa = require('../model/siswa');
 
 const getAll = async (req, res) => {
     try {
-        const siswa = await siswa.find();
-        res.status(200).json(siswa);
+        const data = await siswa.find();
+        res.status(200).json(data);
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Server Error' });
@@ -26,7 +26,40 @@ const create = async (req, res) => {
     }
 }
 
+const getId = async (req,res) => {
+    try {
+        const data = await siswa.findById(req.params.id);
+        res.status(200).json(data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+}
+
+const update = async (req,res) => {
+    try {
+        const data = await siswa.findByIdAndUpdate(req.params.id, req.body, {new: true});
+        res.status(200).json(data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error'});
+    }
+}
+
+const hapus = async (req,res) => {
+    try {
+        const data = await siswa.findByIdAndDelete(req.params.id);
+        res.status(200).json(data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error'});
+    }
+}
+
 module.exports = {
     getAll,
-    create
+    create,
+    getId,
+    update,
+    hapus
 }
--- Express/routes/index.js
+++ Express/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAll, create  } = require('../controllers/controller')
+const { getAll, create, getId, update, hapus } = require('../controllers/controller')
 
 router.get('/', (req, res) => {
     res.send('Hello World!');
@@ -9,5 +9,8 @@ router.get('/', (req, res) => {
 
 router.get('/getall', getAll);
 router.post('/create', create);
+router.get('/getid/:id', getId);
+router.put('/update/:id', update);
+router.delete('/hapus/:id', hapus);
 
 module.exports = router;
